b76f292d25637c7e9e6944910751265e999006ad
aasthatam <taastha22@tbc.edu.np>
2025-01-08 18:26:50 +0545

    Added post model with relationship, scopes, and custom attributes

--- 77356855_component_2_project/app/Models/Post.php
+++ 77356855_component_2_project/app/Models/Post.php
@@ -2,9 +2,45 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $table = ('posts');
+    protected $guarded = [];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::deleting(function ($post)
+        {
+
+        });
+
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+
+    }
+    public function scopeIsPublished($query)
+    {
+        return $query->where('is_published', true);
+    }
+
+    public function scopeIsDrafted($query)
+    {
+        return $query->where('is_published', false);
+    }
+
+    public function getPublishedAttribute()
+    {
+        return ($this->is_published) ? 'Yes' : 'No';
+    }
+    public function getEtagAttribute()
+    {
+        return hash ('sha256', "product-{$this->id}-{$this->updated_at}");
+    }
 }
