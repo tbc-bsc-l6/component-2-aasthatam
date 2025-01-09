de74609150bcc27ecd3871fd1a31d8db7d43da80
aasthatam <taastha22@tbc.edu.np>
2025-01-09 22:00:42 +0545

    Added bootstrap.css

--- 77356855_component_2_project/webpack.mix.js
+++ 77356855_component_2_project/webpack.mix.js
@@ -1,6 +1,7 @@
 const mix = require("laravel-mix");
 mix.styles(
     [
+        "node_modules/bootstrap/dist/css/bootstrap.min.css",
         "resources/css/app.css"
     ],
     "public/css/app.css"
