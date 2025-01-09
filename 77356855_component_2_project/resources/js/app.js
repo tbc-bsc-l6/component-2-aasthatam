a7b3ad9bdb8b93139354414984e73afdd72bb992
aasthatam <taastha22@tbc.edu.np>
2025-01-09 15:41:57 +0545

    Added Bootstrap, Laravel Mix and updated asset files

--- 77356855_component_2_project/resources/js/app.js
+++ 77356855_component_2_project/resources/js/app.js
@@ -1,7 +1,11 @@
-import './bootstrap';
+// import './bootstrap';
 
-import Alpine from 'alpinejs';
+// import Alpine from 'alpinejs';
 
-window.Alpine = Alpine;
+// window.Alpine = Alpine;
 
-Alpine.start();
+// Alpine.start();
+
+import "@popperjs/core";
+window.$=window.jQuery = require("jquery");
+window.bootstrap = require("bootstrap");
---
+++ 77356855_component_2_project/webpack.mix.js
@@ -0,0 +1,15 @@
+const mix = require("laravel-mix");
+mix.styles(
+    [
+        "resources/css/app.css"
+    ],
+    "public/css/app.css"
+).version();
+
+
+mix.js(
+    [
+        "resources/js/app.js"
+    ],
+    "public/js/app.js"
+).version();
