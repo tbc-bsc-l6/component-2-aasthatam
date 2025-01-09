8bc527c2cbea41e97b8a18bc616880aca66bcc55
aasthatam <taastha22@tbc.edu.np>
2025-01-09 23:39:33 +0545

    Implemented shared navbar component with basic layout

---
+++ 77356855_component_2_project/resources/views/layouts/shared/navbar.blade.php
@@ -0,0 +1,15 @@
+<div class="row g-0">
+    <div class="col-12 g-0 p-2 ps-1 pe-1 m-0">
+        <nav class="navbar navbar-light mt-2 mb-2 p-0">
+            <div class="container-fluid ">
+                <a class="navbar-brand d-flex align-items-center" href="/">
+                    <img style="max-height: 70px; max-width: 70px;" src="/img/logo.png" title="Tasty Bites">
+                <a class="" href="/login"><b>Login</b></a>
+
+            </div>
+        </nav>
+        <div class="col-12 text-center g-0 border-bottom-black border-top-black">
+            <img style="width: 100%; height: 430px; object-fit: cover;" src="/img/receipe.jpg" title="Blog Main Image">
+         </div>
+    </div>
+   </div>
