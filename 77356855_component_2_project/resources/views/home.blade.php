6f73474fd6536a660e9bc38b8812512dae362a71
aasthatam <taastha22@tbc.edu.np>
2025-01-09 23:49:01 +0545

    Implemented basic homepage structure with navbar and content section

--- 77356855_component_2_project/resources/views/home.blade.php
+++ 77356855_component_2_project/resources/views/home.blade.php
@@ -1,14 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
-    <title>Home</title>
+    @include('layouts.shared.header')
+
 </head>
-<body>
-   @include('components.header')
+<body style="background-color: #F8F9F9">
+
+   @include('layouts.shared.navbar') 
+   
+   <div class="container ps-5 pe-5">
+    <div class="row">
+        <div class="col-12 p-2 text-center mt-4 mb-4 border-bottom-black">
+        <h1 style='font-family: "Trebuchet MS", sans-serif;'>Tasty Bites</h1>
+        <p style='font-family: "Trebuchet MS", sans-serif;'>Delicious recipes, made easy – Tasty Bites brings the flavors you love to your kitchen!</p>
+        </div>
+        
+    </div>
+</div>
+  
+   <script src ="{{ asset ('js/app.js')}}"></script>
     
 </body>
 </html>
