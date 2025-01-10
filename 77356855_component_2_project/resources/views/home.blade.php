43de947ad3e33a348006c5848d433895b2335073
aasthatam <taastha22@tbc.edu.np>
2025-01-10 22:29:18 +0545

    Imported font from googlefonts for header and description in home.blade.php

--- 77356855_component_2_project/resources/views/home.blade.php
+++ 77356855_component_2_project/resources/views/home.blade.php
@@ -11,8 +11,8 @@
    <div class="container ps-5 pe-5">
     <div class="row">
         <div class="col-12 p-2 text-center mt-4 mb-4 border-bottom-black">
-        <h1 style='font-family: "Trebuchet MS", sans-serif;'>Tasty Bites</h1>
-        <p style='font-family: "Trebuchet MS", sans-serif;'>Delicious recipes, made easy – Tasty Bites brings the flavors you love to your kitchen!</p>
+        <h1 style='font-family: "Dancing Script", cursive;'>Tasty Bites</h1>
+        <p style='font-family: "Lora", serif;'>Delicious recipes, made easy – Tasty Bites brings the flavors you love to your kitchen!</p>
         </div>
         
     </div>
--- 77356855_component_2_project/resources/views/layouts/shared/header.blade.php
+++ 77356855_component_2_project/resources/views/layouts/shared/header.blade.php
@@ -3,3 +3,5 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 <title>{{ $title }}</title>
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500&display=swap" rel="stylesheet">
