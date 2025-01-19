8276e02efaddb8acd699750382278d06aa3b5e78
aasthatam <taastha22@tbc.edu.np>
2025-01-19 21:31:28 +0545

    Extracted common datatable logic from UsersDatatableRepository into DatatableRepository.

--- 77356855_component_2_project/app/Modules/Users/UsersDatatableRepository.php
+++ 77356855_component_2_project/app/Modules/Users/UsersDatatableRepository.php
@@ -3,10 +3,9 @@
 declare(strict_types=1);
 
 namespace App\Modules\Users;
-use Illuminate\Support\Facades\DB;
-use App\Models\User;
+use App\Modules\Datatable\DatatableRepository;
 
-class UsersDatatableRepository
+class UsersDatatableRepository extends DatatableRepository
 {
     protected string $table = "users";
     protected array $orderColumns = [
@@ -31,46 +30,6 @@ class UsersDatatableRepository
     protected string $joinQuery = "";
     protected string $where = "users.id > 0";
 
-    public function index(
-        array $columns,
-        int $start,
-        int $length,
-        array $order,
-        array $search
-    ): array
-    {
-        $query = $this->buildSQL(
-            $columns, $start, $length, $order, $search
-        );
-        $result = json_decode(json_encode(DB::select($query["sql"], $query["bindings"])), true);
 
-        return[
-            "recordsFiltered" => json_decode(json_encode(DB::selectOne($query["countSql"], $query["bindings"])), true)["total"],
-            "recordsTotal" => User::count(),
-            "data" => $result
 
-        ];
-    }
-
-    private function buildSQL(
-        array $columns,
-        int $start,
-        int $length,
-        array $order,
-        array $search
-    ): array
-    {
-        $selectColumns = implode(",", $this->selectColumns);
-        return[
-            "sql" => "SELECT $selectColumns
-                     FROM $this->table
-                     LIMIT $length OFFSET $start",
-            "countSql" => "SELECT COUNT(*) as total
-                     FROM $this->table
-                     LIMIT $length OFFSET $start",
-            "bindings" => []
-        ];
-
-    }
-    
 }
