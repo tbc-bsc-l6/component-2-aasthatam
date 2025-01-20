cfea2ec43338381c88772ae52c5b44b13cc34396
aasthatam <taastha22@tbc.edu.np>
2025-01-20 22:44:18 +0545

    Implemented routes in web.php

--- movie_review_app/routes/web.php
+++ movie_review_app/routes/web.php
@@ -3,12 +3,10 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MovieController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
+Route::get('/',[HomeController::class,'index'])->name('home');
 
 Route::group(['prefix' => 'account'], function(){
         Route::group(['middleware'=> 'guest'], function(){
@@ -22,5 +20,11 @@ Route::group(['prefix' => 'account'], function(){
         Route::get('profile', [AccountController::class, 'profile'])->name('account.profile');
         Route::get('logout', [AccountController::class, 'logout'])->name('account.logout');
         Route::post('update-profile', [AccountController::class, 'updateProfile'])->name('account.updateProfile');
+        Route::get('movies', [MovieController::class, 'index'])->name('movies.index');
+        Route::get('movies/create', [MovieController::class, 'create'])->name('movies.create');
+        Route::post('movies', [MovieController::class, 'store'])->name('movies.store');
+        Route::get('movies/edit/{id}', [MovieController::class, 'edit'])->name('movies.edit');
+        Route::post('movies/edit/{id}', [MovieController::class, 'update'])->name('movies.update');
+        Route::delete('movies', [MovieController::class, 'destroy'])->name('movies.destroy');
     });
 });
